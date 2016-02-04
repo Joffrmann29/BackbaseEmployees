9f70fc20340486e3b5e8419a694243e40273e80a
Joffrey Mann <Joffrmann25@gmail.com>
2016-02-04 11:00:14 -0500

    Model Done
    The data model has been done.

---
+++ BackbaseEmployees/Employee.swift
@@ -0,0 +1,17 @@
+//
+//  Employee.swift
+//  BackbaseEmployees
+//
+//  Created by Joffrey Mann on 2/4/16.
+//  Copyright © 2016 Joffrey Mann. All rights reserved.
+//
+
+import Foundation
+
+class Employee
+{
+    var name = ""
+    var surName = ""
+    var email = ""
+    var role = ""
+}
